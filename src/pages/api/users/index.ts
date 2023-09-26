e07e403035a35734885f4455e3f3654d4e853a7c
Matheus Pereira <matheussbp2014@gmail.com>
2023-09-26 17:19:45 -0300

    Prisma Client: Introdução

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,8 +1,28 @@
 import { NextApiHandler } from 'next'
 import { Users } from '@/utils/users'
+import prisma from '../../../../libs/prisma'
+
+// Getting all users
+const handlerGet: NextApiHandler = (req, res) => {
+  const { search, age } = req.query
+  res.status(200).json(Users)
+}
+
+const handlerPost: NextApiHandler = (req, res) => {
+  const { name, age } = req.body
+
+  res.status(201).json({ status: true, user: { name, age } })
+}
 
 const handler: NextApiHandler = (req, res) => {
-  res.json(Users)
+  switch (req.method) {
+    case 'GET':
+      handlerGet(req, res)
+      break
+    case 'POST':
+      handlerPost(req, res)
+      break
+  }
 }
 
 export default handler
