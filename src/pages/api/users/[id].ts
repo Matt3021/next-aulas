d6ff994e0702008314efc9e628003a3650a1cfc8
Matheus Pereira <matheussbp2014@gmail.com>
2023-10-17 14:25:02 -0300

    Prisma: Deletando dados

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -2,7 +2,7 @@ import { NextApiHandler } from 'next'
 import { Users } from '@/utils/users'
 import prisma from '../../../../libs/prisma'
 
-const handler: NextApiHandler = async (req, res) => {
+const handlerGet: NextApiHandler = async (req, res) => {
   const { id } = req.query
 
   const user = await prisma.user.findUnique({
@@ -17,6 +17,59 @@ const handler: NextApiHandler = async (req, res) => {
   }
   
   res.json({ error: 'Usuário não encontrado' })
+}
+
+const handlerPut: NextApiHandler = async (req, res) => {
+  const { name, active } = req.body
+  const { id } = req.query
+
+  let data: {
+    name? : string,
+    active? : boolean
+  } = {}
+
+  if (name) {
+    data.name = name
+  }
+
+  if (active) {
+    switch (active) {
+      case 'true':
+      case '1':
+        data.active = true
+        break
+      case 'false':
+      case '0':
+        data.active = false
+        break
+      
+    }
+  }
+
+  const updateUser = await prisma.user.update({
+    where: {
+      id: parseInt(id as string)
+    },
+    data
+  })
+
+  if (updateUser) {
+    res.json({ status: true, updateUser })
+    return
+  }
+
+  res.json({ error: 'Não foi possível alterar este usuário' })
+}
+
+const handler: NextApiHandler = async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      handlerGet(req, res)
+      break
+    case 'PUT':
+      handlerPut(req, res)
+      break
+  }
 }
 
 export default handler
